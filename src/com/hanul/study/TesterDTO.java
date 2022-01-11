68539aae198b24d4cd8da9388776aad64fc53db0
eor8451 <77501171+eor8451@users.noreply.github.com>
2022-01-11 18:29:43 +0900

    testerDTO update

--- src/com/hanul/study/TesterDTO.java
+++ src/com/hanul/study/TesterDTO.java
@@ -1,5 +1,42 @@
 package com.hanul.study;
 
 public class TesterDTO {
-	
+	private String name;
+	private String id;
+	private String pw;
+
+	public TesterDTO() {
+	}
+
+	public TesterDTO(String name, String id, String pw) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
 }
