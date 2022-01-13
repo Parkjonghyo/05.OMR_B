303c45fb28d9f66c55d181c95c3479d88d8cb9b8
gyu-hyung <94032808+gyu-hyung@users.noreply.github.com>
2022-01-13 10:17:36 +0900

    test_b view1,2,3,4 추가

--- src/com/hanul/study/table.sql
+++ src/com/hanul/study/table.sql
@@ -7,11 +7,15 @@ create table test_b(
 NO number(30) constraint test_no_nn primary key,
 question varchar2(2000) constraint test_question_pk not null,
 ANSWER VARCHAR2(50) constraint test_answer_nn not null check(answer in (1,2,3,4) ),
-subno number(10) constraint test_subno_nn not null 
+subno number(10) constraint test_subno_nn not null, 
+view1 varchar2(500) constraint test_view1_nn not null,
+view2 varchar2(500) constraint test_view2_nn not null,
+view3 varchar2(500) constraint test_view3_nn not null,
+view4 varchar2(500) constraint test_view4_nn not null
 );
 
-commit;
 
+commit;
 
 CREATE SEQUENCE test_seq    --시퀀스
 START WITH 1 
@@ -21,8 +25,10 @@ CYCLE NOCACHE;
 
 
 INSERT INTO test_b          --insert
-VALUES(Test_seq.NEXTVAL,'문제','2');
+VALUES(Test_seq.NEXTVAL,'문제','2','2','보기1~~~','보기2~~~','보기3~~~','보기4~~~');
 
+
+select * from test_b;       
 truncate table test_b;
 drop sequence test_seq;
 
